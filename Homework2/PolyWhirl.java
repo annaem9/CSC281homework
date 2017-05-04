b23ac7153d43afc06214280baa7cbc3b516b508c
Josh McCoy <joshua.allen.mccoy@gmail.com>
2017-05-04 09:58:51 -0400

    graded

--- Homework2/PolyWhirl.java
+++ Homework2/PolyWhirl.java
@@ -1,38 +1,43 @@
 /*
- * 
+ *
  * Name: Chalirina
- * 
+ *
+ * Score: 100
+ * Excellent work!
+ * -5 inconsistent code formatting
+ * +5 for bonus attempt (it draws any of the specified shapes but not shapes of with sides greater than 6)
+ *
  * Grading: 70 points for correct completion of all tasks and 30 points for style, creativity, and
  * following instructions. Up to 10 points can be recovered if you successfully complete the bonus.
- * 
+ *
  * Tasks:
- * 
+ *
  * -Read in shape specifications that consist of 3 numbers: number of sides of the shape, the left
  * coordinate of the center in pixels, and the right coordinate of the center in pixels. There will
  * be one shape per line of input text via standard input.
- * 
+ *
  * -Make a list per shape to store instances of that shape.
- * 
+ *
  * -Read through each shape specification from the input and add a new instance of that shape to the
  * appropriate shape list. For example, the input of 3 100 100 would result in code like the
  * following in the readAndGenerateShapes method of the Surface class: this.tris.add(new Tri(100,
  * 100));
- * 
+ *
  * -Create a set of classes that extend BasicShape with the following number of sides: Tri - 3
  * sided, Quad - 4 sided, Pent - 5 sided, and Hex - 6 sided. Note that these shapes do not need to
  * be regular; each shape can have sides of non-uniform length (e.g. both squares and rectangles are
  * valid Quads.
- * 
+ *
  * -Pick a fill and stroke color for each class. Let your inner graphic designer or color theorist
  * shine through!
- * 
+ *
  * -Each shape should be ~50 pixels wide (i.e. all of the points should be roughly 25 pixels from
  * the center of the shape).
- * 
+ *
  * -Document the classes and methods you create.
- * 
+ *
  * -Document the functions that are declared after comments that contain TODO: document.
- * 
+ *
  * Bonus: Create an additional class that inherits BasicShape that can draw a regular shape with any
  * number of sides.
  */
@@ -93,13 +98,13 @@ public class PolyWhirl extends JFrame {
    *
    * The Surface class inherits JPanel and acts as a drawing surface. It is the core of this program
    * and does the following:
-   * 
+   *
    * -Reads input through the scanner, creates instances of the shapes designated by that input, and adds them to the list of shapes to be drawn.
    * this occurs via the readAndGenerateShapes method, which is recursive, meaning it calls itself. It's recursive because we want to be able to add
    * more than one shape to our list of shapes to draw!
-   * 
+   *
    * -Then Manages the lists of shapes and is responsible for rendering them via the drawShapes method.
-   * 
+   *
    */
 
 	class Surface extends JPanel {
@@ -112,15 +117,15 @@ public class PolyWhirl extends JFrame {
       			this.drawShapes(g);
       			// this.readAndGenerateShapes();
     			}
- 
+
 	Scanner katz= new Scanner(System.in);
- 
+
 	public void readAndGenerateShapes() {
 
-		if (katz.hasNextInt()==false) {  
+		if (katz.hasNextInt()==false) {
  			System.out.println("you're out of pretty shapes--don't forget to close the window before trying again"); //exit the loop
-    			} else{ 
- 
+    			} else{
+
 			int numSides=katz.nextInt();
 			int x=0;
 			int y=0;
@@ -131,15 +136,15 @@ public class PolyWhirl extends JFrame {
 
   				Tri wetri=new Tri(x, y); //a triangle is added to our list of shapes to draw
   				crystals.add(wetri);
-				} 
+				}
 
-				else if (numSides==4) {    
+				else if (numSides==4) {
   				x=katz.nextInt();
    				y=katz.nextInt();
 
  				Quad wequad=new Quad(x, y);
 				crystals.add(wequad);
-  				} 
+  				}
 
 				else if (numSides==5) {
  				x=katz.nextInt();
@@ -147,7 +152,7 @@ public class PolyWhirl extends JFrame {
 
  				Pent wepent=new Pent(x, y);
  				crystals.add(wepent);
-				} 
+				}
 
 				else if (numSides==6){
    				x=katz.nextInt();
@@ -161,7 +166,7 @@ public class PolyWhirl extends JFrame {
 			}
 
 		}
-	public void drawShapes(Graphics g) {      
+	public void drawShapes(Graphics g) {
       		((Graphics2D) g).setColor(java.awt.Color.WHITE); // this sets the color for the background
       		((Graphics2D) g).fillRect(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);  // this creates that background
 
